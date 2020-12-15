8fcfd9d53d3aee97971f3295378259c83a6cf59c
weihaoyu <weihaoyu@ymt360.com>
2020-12-15 11:00:58 +0800

    sys exec

--- libraries/util/sys/sys.go
+++ libraries/util/sys/sys.go
@@ -1,13 +1,48 @@
 package sys
 
 import (
+	"bytes"
 	"errors"
+	"io"
 	"net"
 	"os"
+	"os/exec"
 	"strings"
 
 	utli_err "gin-api/libraries/util/error"
 )
+
+//执行系统命令
+func ExecCommand(name string, arg ...string) string{
+	cmd := exec.Command(name, arg...)
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	_ = cmd.Run()
+
+	return buf.String()
+}
+
+//执行系统pipe命令
+//ps := exec.Command("ps", "-ef")
+//grep := exec.Command("grep", "-i", "php-fpm")
+func ExecCommandGrep(command *exec.Cmd, grep *exec.Cmd) string {
+	r, w := io.Pipe() // 创建一个管道
+	defer r.Close()
+	defer w.Close()
+	command.Stdout = w // command向管道的一端写
+	grep.Stdin = r     // grep从管道的一端读
+
+	var buffer bytes.Buffer
+	grep.Stdout = &buffer // grep的输出为buffer
+
+	_ = command.Start()
+	_ = grep.Start()
+	command.Wait()
+	w.Close()
+	grep.Wait()
+
+	return buffer.String()
+}
 
 func ExternalIP() (string, error) {
 	iFaces, err := net.Interfaces()
