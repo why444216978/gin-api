eed5cbac434af760822377ab4b40322a7d7f7bec
weihaoyu <weihaoyu@ymt360.com>
2021-10-24 19:28:59 +0800

    DDD restructure

--- api/conn/conn.go
+++ api/conn/conn.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"time"
 
-	goods_respository "github.com/why444216978/gin-api/internal/goods/respository"
 	goods_service "github.com/why444216978/gin-api/internal/goods/service"
 	redis_cache "github.com/why444216978/gin-api/library/cache/redis"
 	redis_lock "github.com/why444216978/gin-api/library/lock/redis"
@@ -13,71 +12,15 @@ import (
 	"github.com/why444216978/gin-api/response"
 
 	"github.com/gin-gonic/gin"
-	"github.com/why444216978/go-util/orm"
 	"golang.org/x/sync/errgroup"
 )
 
 func Do(c *gin.Context) {
-	var (
-		err   error
-		goods goods_respository.Test
-	)
-
 	ctx := c.Request.Context()
-	db := resource.TestDB.DB
 
-	db = db.WithContext(ctx).Begin()
-
-	defer func() {
-		if err != nil {
-			db.WithContext(ctx).Rollback()
-			// resource.ServiceLogger.Error
-			response.Response(c, response.CodeServer, goods, err.Error())
-			return
-		}
-		err = db.WithContext(ctx).Commit().Error
-		if err != nil {
-			response.Response(c, response.CodeServer, goods, err.Error())
-			return
-		}
-
-		response.Response(c, response.CodeSuccess, goods, "")
-	}()
-
-	err = db.WithContext(ctx).Select("*").First(&goods).Error
+	goods, err := goods_service.Instance.CrudGoods(ctx)
 	if err != nil {
-		return
-	}
-
-	_, err = orm.Insert(ctx, db, &goods_respository.Test{
-		GoodsId: 333,
-		Name:    "a",
-	})
-	if err != nil {
-		return
-	}
-
-	where := map[string]interface{}{"goods_id": 333}
-	update := map[string]interface{}{"name": 333}
-
-	_, err = orm.Update(ctx, db, &goods_respository.Test{}, where, update)
-	if err != nil {
-		return
-	}
-
-	_, err = orm.Delete(ctx, db, &goods_respository.Test{}, where)
-	if err != nil {
-		return
-	}
-
-	var name string
-	err = db.WithContext(ctx).Table("test").Where("id = ?", 1).Select("name").Row().Scan(&name)
-	if err != nil {
-		return
-	}
-
-	err = db.WithContext(ctx).Raw("select * from test where id = 1 limit 1").Scan(&goods).Error
-	if err != nil {
+		response.Response(c, response.CodeServer, goods, err.Error())
 		return
 	}
 
--- internal/goods/service/service.go
+++ internal/goods/service/service.go
@@ -1,17 +1,20 @@
 package service
 
 import (
+	"context"
 	"strconv"
 
+	"github.com/why444216978/gin-api/internal/goods/respository"
 	"github.com/why444216978/gin-api/resource"
+	"github.com/why444216978/go-util/orm"
 
-	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
 )
 
 type GoodsInterface interface {
-	GetGoodsName(c *gin.Context, id int) (string, error)
+	GetGoodsName(ctx context.Context, id int) (string, error)
+	CrudGoods(ctx context.Context) (goods respository.Test, err error)
 }
 
 var Instance GoodsInterface
@@ -27,8 +30,8 @@ const (
 	GOODS_PRICE_KEY = "goods::price::"
 )
 
-func (srv *GoodsService) GetGoodsName(c *gin.Context, id int) (string, error) {
-	data, err := resource.RedisCache.Get(c.Request.Context(), GOODS_NAME_KEY+strconv.Itoa(id)).Result()
+func (gs *GoodsService) GetGoodsName(ctx context.Context, id int) (string, error) {
+	data, err := resource.RedisCache.Get(ctx, GOODS_NAME_KEY+strconv.Itoa(id)).Result()
 	if err == redis.Nil {
 		err = nil
 	}
@@ -36,4 +39,55 @@ func (srv *GoodsService) GetGoodsName(c *gin.Context, id int) (string, error) {
 		err = errors.Wrap(err, "redis get goods price error：")
 	}
 	return data, err
+}
+
+func (gs *GoodsService) CrudGoods(ctx context.Context) (goods respository.Test, err error) {
+	db := resource.TestDB.DB.WithContext(ctx).Begin()
+
+	defer func() {
+		if err != nil {
+			db.WithContext(ctx).Rollback()
+			return
+		}
+		err = db.WithContext(ctx).Commit().Error
+	}()
+
+	err = db.WithContext(ctx).Select("*").First(&goods).Error
+	if err != nil {
+		return
+	}
+
+	_, err = orm.Insert(ctx, db, &respository.Test{
+		GoodsId: 333,
+		Name:    "a",
+	})
+	if err != nil {
+		return
+	}
+
+	where := map[string]interface{}{"goods_id": 333}
+	update := map[string]interface{}{"name": 333}
+
+	_, err = orm.Update(ctx, db, &respository.Test{}, where, update)
+	if err != nil {
+		return
+	}
+
+	_, err = orm.Delete(ctx, db, &respository.Test{}, where)
+	if err != nil {
+		return
+	}
+
+	var name string
+	err = db.WithContext(ctx).Table("test").Where("id = ?", 1).Select("name").Row().Scan(&name)
+	if err != nil {
+		return
+	}
+
+	err = db.WithContext(ctx).Raw("select * from test where id = 1 limit 1").Scan(&goods).Error
+	if err != nil {
+		return
+	}
+
+	return
 }
